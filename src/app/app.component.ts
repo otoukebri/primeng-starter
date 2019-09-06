aba2782828c48d7c8f91817628ea3cb276bc3cef
Oussema TOUKEBRI <oussema.toukebri.ext@spw.wallonie.be>
2019-09-06 08:14:25 +0200

    p-radioButton example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
-import { Component, OnInit } from '@angular/core';
-import { Car } from './domain/car';
-import { CarService } from './services/carservice';
+import {Component, OnInit} from '@angular/core';
+import {Car} from './domain/car';
+import {CarService} from './services/carservice';
+import {FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
 
 export class PrimeCar implements Car {
-    constructor(public vin?, public year?, public brand?, public color?) {}
+    constructor(public vin?, public year?, public brand?, public color?) {
+    }
 }
 
 @Component({
@@ -14,63 +16,17 @@ export class PrimeCar implements Car {
 })
 export class AppComponent implements OnInit {
 
-    displayDialog: boolean;
+    // selectedValue = 'val2';
 
-    car: Car = new PrimeCar();
+    myForm: FormGroup;
 
-    selectedCar: Car;
-
-    newCar: boolean;
-
-    cars: Car[];
-
-    cols: any[];
-
-    constructor(private carService: CarService) { }
+    constructor(private carService: CarService, private fb: FormBuilder) {
+        this.myForm = this.fb.group({
+            myRadio: this.fb.control('val2', Validators.required)
+        });
+    }
 
     ngOnInit() {
-        this.carService.getCarsSmall().then(cars => this.cars = cars);
-
-        this.cols = [
-            { field: 'vin', header: 'Vin' },
-            { field: 'year', header: 'Year' },
-            { field: 'brand', header: 'Brand' },
-            { field: 'color', header: 'Color' }
-        ];
     }
 
-    showDialogToAdd() {
-        this.newCar = true;
-        this.car = new PrimeCar();
-        this.displayDialog = true;
-    }
-
-    save() {
-        const cars = [...this.cars];
-        if (this.newCar) {
-            cars.push(this.car);
-        } else {
-            cars[this.findSelectedCarIndex()] = this.car;
-        }
-        this.cars = cars;
-        this.car = null;
-        this.displayDialog = false;
-    }
-
-    delete() {
-        const index = this.findSelectedCarIndex();
-        this.cars = this.cars.filter((val, i) => i !== index);
-        this.car = null;
-        this.displayDialog = false;
-    }
-
-    onRowSelect(event) {
-        this.newCar = false;
-        this.car = {...event.data};
-        this.displayDialog = true;
-    }
-
-    findSelectedCarIndex(): number {
-        return this.cars.indexOf(this.selectedCar);
-    }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { InputTextModule } from 'primeng/inputtext';
 import { ButtonModule } from 'primeng/button';
@@ -9,20 +9,23 @@ import { TableModule } from 'primeng/table';
 import { DialogModule } from 'primeng/dialog';
 
 import { AppComponent } from './app.component';
+import {RadioButtonModule} from 'primeng/radiobutton';
 
 @NgModule({
     declarations: [
-        AppComponent
+        AppComponent,
     ],
     imports: [
         BrowserModule,
         BrowserAnimationsModule,
         FormsModule,
+        ReactiveFormsModule,
         TableModule,
         HttpClientModule,
         InputTextModule,
         DialogModule,
-        ButtonModule
+        ButtonModule,
+        RadioButtonModule
     ],
     providers: [],
     bootstrap: [AppComponent]
